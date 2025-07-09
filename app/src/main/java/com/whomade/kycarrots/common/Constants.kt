6ef6661ca3161cf6cc23d287131bf9827ba52d07
SeoSooHyun <seo3997@gmail.com>
2025-07-08 22:35:39 -0400

    무한스크롤

--- app/src/main/java/com/whomade/kycarrots/common/Constants.kt
+++ app/src/main/java/com/whomade/kycarrots/common/Constants.kt
@@ -1,8 +1,8 @@
 package com.whomade.kycarrots.common
 
 object Constants {
-    //const val BASE_URL = "http://52.231.229.156:9000/"
-    const val BASE_URL = "http://10.118.52.129:9000/"
+    const val BASE_URL = "http://52.231.229.156:9000/"
+    //const val BASE_URL = "http://10.118.52.129:9000/"
     const val BASE_CHAT_URL = "ws://52.231.229.156/chat-ws/websocket"
     //const val BASE_CHAT_URL = "ws://172.30.1.20:9000/chat-ws/websocket"
 }
