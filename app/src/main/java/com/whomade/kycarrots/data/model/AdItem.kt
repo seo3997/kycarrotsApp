3275c4d6b46765b9e578a7c0d5ea94086a650502
soohyunSeo <seo3997@gmail.com>
2026-02-22 00:44:30 +0900

    주문내역 개발중

--- app/src/main/java/com/whomade/kycarrots/data/model/AdItem.kt
+++ app/src/main/java/com/whomade/kycarrots/data/model/AdItem.kt
@@ -1,10 +1,18 @@
 package com.whomade.kycarrots.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class AdItem(
+    @SerializedName(value = "productId", alternate = ["PRODUCT_ID"])
     val productId: String,
+    @SerializedName(value = "title", alternate = ["TITLE"])
     val title: String,
+    @SerializedName(value = "description", alternate = ["PRODUCT_NAME"])
     val description: String,
+    @SerializedName(value = "price", alternate = ["TOTAL_PAY_AMOUNT"])
     val price: String,
+    @SerializedName(value = "imageUrl", alternate = ["IMAGE_URL"])
     val imageUrl: String,
+    @SerializedName(value = "userId", alternate = ["USER_NO"])
     val userId: String,
 )
--- app/src/main/java/com/whomade/kycarrots/data/model/AdResponse.kt
+++ app/src/main/java/com/whomade/kycarrots/data/model/AdResponse.kt
@@ -1,5 +1,8 @@
 package com.whomade.kycarrots.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class AdResponse(
+    @SerializedName(value = "items", alternate = ["content"])
     val items: List<AdItem>
 )
